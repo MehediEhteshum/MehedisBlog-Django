6a48627392bf4a59a35980ac4ef03cefb2e024a6
Mehedi Ehteshum <me69oshan@gmail.com>
2021-01-03 14:58:22 -0700

    blog app views updated;

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
+# dummy posts from db
+posts = [
+    {
+        "author": "Mehedi Ehteshum",
+        "title": "Post 1",
+        "content": "This is the first post.",
+        "date_posted": "January 1, 2021"
+    },
+    {
+        "author": "Mansura Khanom",
+        "title": "Post 2",
+        "content": "This is the second post.",
+        "date_posted": "January 3, 2021"
+    },
+    {
+        "author": "Md Azim",
+        "title": "Post 3",
+        "content": "HTML khub moja.",
+        "date_posted": "January 3, 2021"
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "title": "Home",
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
     return render(request, "blog/about.html")
