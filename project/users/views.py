679f6137ed939c13442c98ad37abe05cc4f2354c
Mehedi Ehteshum <me69oshan@gmail.com>
2021-01-04 16:59:54 -0700

    user registration form updated with email field;

--- project/users/views.py
+++ project/users/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import UserSignupForm
+from django.contrib import messages
 
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            form.save() # creates and saves user
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+            return redirect("blog-home")
+    else:
+        form = UserSignupForm()
     context = {"form": form}
     return render(request, "users/signup.html", context)
