d5241dda3b34fa1ebea606aa038f22e3c057ff46
Mehedi Ehteshum <me69oshan@gmail.com>
2021-01-03 20:55:26 -0700

    post model created;

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
-# dummy posts from db
-posts = [
-    {
-        "author": "Mehedi Ehteshum",
-        "title": "Post 1",
-        "content": "This is the first post.",
-        "date_posted": "January 1, 2021"
-    },
-    {
-        "author": "Mansura Khanom",
-        "title": "Post 2",
-        "content": "This is the second post.",
-        "date_posted": "January 3, 2021"
-    },
-    {
-        "author": "Md Azim",
-        "title": "Post 3",
-        "content": "HTML khub moja.",
-        "date_posted": "January 3, 2021"
-    }
-]
+# # dummy posts from db
+# posts = [
+#     {
+#         "author": "Mehedi Ehteshum",
+#         "title": "Post 1",
+#         "content": "This is the first post.",
+#         "date_posted": "January 1, 2021"
+#     },
+#     {
+#         "author": "Mansura Khanom",
+#         "title": "Post 2",
+#         "content": "This is the second post.",
+#         "date_posted": "January 3, 2021"
+#     }
+# ]
 
 # Create your views here.
 def home(request):
+    posts = Post.objects.all()
     context = {
         "title": "Home",
         "posts": posts
