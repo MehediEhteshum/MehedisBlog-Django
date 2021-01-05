7a7152f37c0a60ca34c201fd8c42b4a55233267c
Mehedi Ehteshum <me69oshan@gmail.com>
2021-01-04 17:36:44 -0700

    sign up form updated using crispy form;

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'crispy_forms',
+
+    # apps
     'blog',
     'users',
 ]
@@ -128,3 +132,5 @@ STATIC_ROOT = BASE_DIR/"cdn_test"/"static"
 
 if DEBUG:
     STATIC_ROOT.mkdir(parents=True, exist_ok=True)
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
